2f8b0b2f0a0c3748ee675e70b25a9a10fbfe1b8c
rbrush18 <33459264+rbrush18@users.noreply.github.com>
2017-11-14 10:51:51 -0500

    Add files via upload

--- madlib_RB.py
+++ madlib_RB.py
@@ -1,3 +1,4 @@
+import time
 ### MAD LIBS ###
 ### Source: https://i.pinimg.com/originals/ea/52/fa/ea52fa58d6410047a823b5b2efa57fb8.jpg
 
@@ -66,18 +67,20 @@ noun8 = input()
 ### Begin Mad Lib ###
 
 print("Hi!")
-print("This is " + person1 + ", speaking to you from the broadcasting " + noun1 + "at the " + adjective1 + "forum.")
-print("In case you " + pasttenseverb1 + "in late, the score between the Los Angeles " + pluralnoun1 + "and the Boston " + pluralnoun2 + "is a squeaker, 141 to " + number1 + ".")
-print("This has been the most " + adjective2 + "game these " + adjective3 + "eyes has seen in years.")
-print("First, one team scores a " + noun2 + ", then " + exclamation1 + "! - the other team comes right back.")
+print("This is " + person1 + " , speaking to you from the broadcasting " + noun1 + " at the " + adjective1 + " forum.")
+print("In case you " + pasttenseverb1 + " in late, the score between the Los Angeles " + pluralnoun1 + " and the Boston " + pluralnoun2 + " is a squeaker, 141 to " + number1 + " .")
+print("This has been the most " + adjective2 + " game these " + adjective3 + " eyes has seen in years.")
+print("First, one team scores a " + noun2 + " , then " + exclamation1 + " ! - the other team comes right back.")
 print("Okay.")
 print("Time-out is over.")
-print("Los Angeles brings in the ball at mid-" + noun3 + " .")
-print(person2 + " dribbles down the " + noun4 + ", fakes the defender out of his " + noun5 + "and shoots a " + number2 + "-handed shot.")
+print("Los Angeles brings in the ball at mid- " + noun3 + " .")
+print(person2 + " dribbles down the " + noun4 + ", fakes the defender out of his " + noun5 + " and shoots a " + number2 + " -handed shot.")
 print("It goes right through the " + noun6 + ".")
-print("He beat the " + noun7 + "!")
-print("the game is over just as the " + noun8 + "goes off.")
+print("He beat the " + noun7 + " !")
+print("the game is over just as the " + noun8 + " goes off.")
 
+
+time.sleep(300)
 
 
 
